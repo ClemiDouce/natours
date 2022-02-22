00dc73f5c0d89c4f2fa1bdabbb23ded8c36d8c67
Clementine <your@email.com>
2022-02-22 14:15:23 +0100

    Add tour schema

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 
+const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
 app.use(express.json());
-app.use(morgan("dev"));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- server.js
+++ server.js
@@ -1,6 +1,47 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
 
-const port = 3000;
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful");
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, " a tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: { type: Number, required: [true, "a tour must have a price"] },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "Grand theatre",
+  rating: 4.3,
+  price: 200,
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
